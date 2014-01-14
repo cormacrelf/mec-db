d2347a7e5164ede3de0f1938e50905aefa7abb9d
Cormac Relf <cormacrelf@me.com>
2014-01-15 00:42:37 +1100

    peer list: keep zmq interfaces to other peers

---
+++ peers/peers.go
@@ -0,0 +1,77 @@
+package peers
+
+import (
+	zmq "github.com/pebbe/zmq4"
+	ml "github.com/hashicorp/memberlist"
+	"fmt"
+// 	"code.google.com/p/go-uuid/uuid"
+// 	"bytes"
+// 	"errors"
+// 	"net"
+// 	"time"
+)
+
+type PeerList struct {
+	ml.EventDelegate
+	router *zmq.Socket
+	dealers map[string]*zmq.Socket
+}
+
+func Create(port int) *PeerList {
+	r, err := zmq.NewSocket(zmq.ROUTER)
+	if err != nil {
+		panic("Can't create ROUTER socket")
+	}
+	addr := fmt.Sprintf("tcp://*:%d", port)
+	err = r.Bind(addr)
+	if err != nil {
+		panic(fmt.Sprintf("Can't bind router on port %d", port))
+	}
+	pl := &PeerList {
+		router: r,
+		dealers: make(map[string]*zmq.Socket, 100),
+	}
+
+	go pl.receive()
+
+	return pl
+}
+
+func (p *PeerList) NotifyJoin(node *ml.Node) {
+	fmt.Printf("JOINED: %v, %v:%d\n", node.Name, node.Addr, node.Port)
+	sock, err := zmq.NewSocket(zmq.DEALER)
+	if err != nil {
+		panic("Can't create DEALER socket")
+	}
+
+	addr := fmt.Sprintf("tcp://%s:%d", node.Addr.String(), node.Port + 1)
+	err = sock.Connect(addr)
+	if err != nil {
+		panic(fmt.Sprintf("Can't connect to router at %s", addr))
+	}
+
+	(*p).dealers[node.Name] = sock
+	(*p).Message(node.Name, "HELLO")
+}
+
+func (p *PeerList) NotifyLeave(node *ml.Node) {
+	delete((*p).dealers, node.Name)
+}
+
+func (p PeerList) receive() {
+	for {
+		data, err := p.router.RecvMessage(0)
+		if err != nil {
+			fmt.Printf("router err %v\n", err)
+		}
+		fmt.Printf("%v\n", data[1])
+	}
+}
+
+func (p PeerList) Message(recipient, msg string) {
+	dest := p.dealers[recipient]
+	_, err := dest.SendMessage(msg)
+	if err != nil {
+		fmt.Printf("dealer error %v\n", err)
+	}
+}
